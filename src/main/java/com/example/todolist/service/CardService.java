7bda24957310aa9a2d3dcc7704de614e74dbcd16
tmdgusya <tmdgusya@naver.com>
2021-07-09 22:03:01 +0900

    [#8] feat:  카드 수정 기능 추가

--- src/main/java/com/example/todolist/service/CardService.java
+++ src/main/java/com/example/todolist/service/CardService.java
@@ -2,7 +2,9 @@ package com.example.todolist.service;
 
 import com.example.todolist.domain.Card;
 import com.example.todolist.domain.Column;
-import com.example.todolist.dto.CardRequestDTO;
+import com.example.todolist.dto.CardAddRequestDTO;
+import com.example.todolist.dto.CardUpdateRequestDto;
+import com.example.todolist.exception.CardNotFoundException;
 import com.example.todolist.exception.ColumnNotFoundException;
 import com.example.todolist.repository.CardRepository;
 import com.example.todolist.repository.ColumnRepository;
@@ -19,9 +21,18 @@ public class CardService {
         this.columnRepository = columnRepository;
     }
 
-    public void addCard(Long columnId, CardRequestDTO cardRequest) {
+    public void addCard(Long columnId, CardAddRequestDTO cardRequest) {
         Column column = columnRepository.findById(columnId).orElseThrow(ColumnNotFoundException::new);
         Card card = new Card(cardRequest.getTitle(), cardRequest.getContent(), cardRequest.getAuthor(), column);
         cardRepository.save(card);
+    }
+
+    public Card updateCard(Long columnId, Long cardId, CardUpdateRequestDto cardUpdateRequestDto) {
+        Column column = columnRepository.findById(columnId).orElseThrow(ColumnNotFoundException::new);
+        Card card = cardRepository.findById(cardId).orElseThrow(CardNotFoundException::new);
+
+        card.update(cardUpdateRequestDto.getTitle(), cardUpdateRequestDto.getContent(), column);
+
+        return cardRepository.save(card);
     }
 }
