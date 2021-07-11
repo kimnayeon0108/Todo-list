ed0d4aa4a8f65d5816d2ad1a402471485631f1bd
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-07-11 17:10:59 +0900

    [#10] feat: 로그 생성 기능 테스트가 동작하도록 기능 추가

--- src/main/java/com/example/todolist/service/LogService.java
+++ src/main/java/com/example/todolist/service/LogService.java
@@ -1,12 +1,22 @@
 package com.example.todolist.service;
 
+import com.example.todolist.domain.Actions;
 import com.example.todolist.domain.Card;
+import com.example.todolist.domain.Log;
+import com.example.todolist.repository.LogRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LogService {
 
-    public void createLog(Card any) {
+    private final LogRepository logRepository;
 
+    public LogService(LogRepository logRepository) {
+        this.logRepository = logRepository;
+    }
+
+    public void createLog(Card card) {
+        Log log = new Log(card.getAuthor(), Actions.ENROLL, card.getColumn(), card.getColumn(), card);
+        logRepository.save(log);
     }
 }
