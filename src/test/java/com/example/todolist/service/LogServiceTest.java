5f94e01b137885101ad5a8ba71d6cbf49d1fa774
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-07-11 17:10:11 +0900

    [#10] test: 로그 생성 기능 테스트 추가

---
+++ src/test/java/com/example/todolist/service/LogServiceTest.java
@@ -0,0 +1,40 @@
+package com.example.todolist.service;
+
+import com.example.todolist.domain.Card;
+import com.example.todolist.domain.Log;
+import com.example.todolist.repository.LogRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+public class LogServiceTest {
+
+    @Autowired
+    private LogService logService;
+
+    @MockBean
+    private Log log;
+
+    @MockBean
+    private Card card;
+
+    @MockBean
+    private LogRepository logRepository;
+
+    @Test
+    @DisplayName("로그 추가 기능 테스트")
+    void createLog() {
+
+        logService.createLog(card);
+
+        verify(logRepository, times(1)).save(any(Log.class));
+    }
+}
